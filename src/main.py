93683c9d05860ef92756501c1e92363cdf0e21a9
Mayank Gupta <techfreakworm@gmail.com>
2019-12-17 10:53:02 +0530

    Better news count tracking

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 from newsapi import DataGatherer
+from util.log import Logger
+from datetime import datetime
+from dateutil.tz import *
 
+
+Logger.info('=====================Job Starting at: ' + str(datetime.now().astimezone(tzlocal())))
 DataGatherer().indian_business_news()
+Logger.info('=====================Job Completed at: ' + str(datetime.now().astimezone(tzlocal())))
--- src/newsapi/data_gatherer.py
+++ src/newsapi/data_gatherer.py
@@ -5,6 +5,9 @@ import math
 from bson.objectid import ObjectId
 import hashlib
 from util.log import Logger
+from datetime import datetime
+from dateutil.tz import *
+from pymongo.errors import DuplicateKeyError
 
 
 class DataGatherer:
@@ -13,12 +16,15 @@ class DataGatherer:
         self.api_key = Config.NEWS_API_KEY
         self.client = MongoAdapter().get_mongo_client()
         self.news_collection = self.client[Config.MONGO_DB][Config.NEWS_COLLECTION]
+        self.inserted_articles = 0
+        self.total_articles = 0
+        self.not_inserted_articles = 0
 
     def indian_business_news(self):
         res = requests.get(self.business_url + '&pageSize=100&apiKey=' + self.api_key).json()
         total_results = res['totalResults']
+        self.total_articles = int(total_results)
 
-        # TODO: create Id by hashing the titles so that there are no duplicate news
         # 100 is the max pagesize for newsapi.org
         if total_results <=100:
             self.insert_data_into_db(res['articles'])
@@ -32,11 +38,19 @@ class DataGatherer:
                     '&pageSize=10&page={}&apiKey='.format(page) + \
                     self.api_key).json()
                 self.insert_data_into_db(res['articles'])
-        Logger.info('News data stored successfully!', push_to_slack=True)
+        if self.inserted_articles > 0:
+            Logger.info('News data stored successfully!\nTotal articles: {}\nInserted articles: {}'.format(self.total_articles, self.inserted_articles), push_to_slack=True)
+        else:
+            Logger.info('Total articles: {}\nNot inserted articles: {}'.format(self.total_articles, self.not_inserted_articles))
                 
 
     def insert_data_into_db(self, articles: list):
+        self.total_articles = len(articles)
         for article in articles:
             article['_id'] = ObjectId(hashlib.md5(article['title'].encode()).hexdigest()[8:])
-            self.news_collection.insert_one(article)
-
+            article['createdDate'] = datetime.now().astimezone(tzlocal())
+            try:
+                self.news_collection.insert_one(article)
+                self.inserted_articles = self.inserted_articles + 1
+            except DuplicateKeyError as err:
+                self.not_inserted_articles = self.not_inserted_articles + 1
