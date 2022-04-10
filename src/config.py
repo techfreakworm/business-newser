07c235abaf0c0f346833461296490f2a03e64ab4
Mayank Gupta <techfreakworm@gmail.com>
2022-04-10 16:34:46 +0530

    Update additional requirements for selenium

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import sys
 
 
 load_dotenv()
@@ -13,3 +14,10 @@ class Config:
     INDIAN_BNEWS_URL_BASE = 'https://newsapi.org/v2/top-headlines?country=in&category=business'
     SLACK_WEBHOOK = os.getenv('SLACK_WEBHOOK')
 
+
+    ## SELENIUM
+    SELENIUM_DRIVER_BASE_PATH = os.getenv('SELENIUM_DRIVER_LOCATION')
+    SELENIUM_DRIVER_CHROME_EXEC_PATH = \
+        os.path.join(SELENIUM_DRIVER_BASE_PATH, 'chromedriver_'+sys.platform)
+    SELENIUM_DRIVER_FIREFOX_EXEC_PATH = \
+        os.path.join(SELENIUM_DRIVER_BASE_PATH, 'geckodriver_'+sys.platform)
